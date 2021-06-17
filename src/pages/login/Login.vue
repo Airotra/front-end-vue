d841b2d1ef01dc7f4013b08364b2786f21b2acf0
Airotra <Airotra@users.noreply.github.com>
2021-06-17 14:17:28 +0800

    创建初始项目

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,5 +1,18 @@
 <template>
-    <p>this is login html</p>
+    <div id="app">
+        <el-container class="continer" style="height: 100%">
+            <el-aside class="left" style="width: 400px; border: 1px solid black">
+                <img src="static/images/icon/shop.png" style="transform:scale(0.5)">
+                <h2 style="left: 50px;top: -30px;margin: 0;position: relative;font-weight: lighter">欢迎使用</h2>
+                <h2 style="left: 150px;top: -15px;margin: 0;position: relative;font-weight: normal">XX购物商城</h2>
+            </el-aside>
+
+            <el-main class="right" style="">
+
+            </el-main>
+        </el-container>
+
+    </div>
 </template>
 
 <script>

d12a44902436ff9a3a2f1ad6a1a4ed284a05445f
Airotra <Airotra@users.noreply.github.com>
2021-06-22 10:43:52 +0800

    2021.6.22，个人信息页面前端设计1

--- src/pages/main/user/UserInfo.vue
+++ src/pages/main/user/UserInfo.vue
@@ -9,6 +9,38 @@
             </div>
         </el-card>
         <el-card class="container">
+            <div class="topContainer">
+                <p class="accountFont" >我的账户</p>
+            </div>
+            <div style="width: 90%; margin-left: 5%">
+                <div class="leftContainer">
+                    <div style="border-bottom: 1px solid rgb(240,240,240)">
+                        <p style="margin: 4%; font-size: 1.1em;">个人基本信息</p>
+                    </div>
+                    <div class="avatar">
+                        <el-avatar icon="el-icon-user-solid" :size="100" style="margin: 10px 36%;"></el-avatar>
+                    </div>
+                    <div class="userInfo">
+                        <div style="border-bottom: 1px solid rgb(240,240,240)">
+                            <p style="margin-left: 5%">昵称： {{nickName}}</p>
+                        </div>
+                        <div style="border-bottom: 1px solid rgb(240,240,240)">
+                            <p style="margin-left: 5%">手机号： {{phoneNumber}}</p>
+                        </div>
+                        <div style="border-bottom: 1px solid rgb(240,240,240)">
+                            <p style="margin-left: 5%">积分： {{point}}</p>
+                        </div>
+                        <div style="border-bottom: 1px solid rgb(240,240,240)">
+                            <p style="margin-left: 5%">用户类型： 普通用户</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="rightContainer">
+                    <div style="border-bottom: 1px solid rgb(240,240,240)">
+                        <p style="margin: 4%; font-size: 1.1em;">常用地址</p>
+                    </div>
+                </div>
+            </div>
 
         </el-card>
     </div>
@@ -19,9 +51,19 @@
 
     export default {
         name: 'UserInfo',
+        data () {
+            return {
+                nickName: '',
+                phoneNumber: '',
+                avatar: '',
+                address: '',
+                point: ''
+            }
+        },
         computed: {
             ...mapGetters({
                 userType: 'user/getUserType',
+                userId: 'user/getUserId',
                 getSidebarList: 'sidebar/getSidebarList'
             })
         },
@@ -38,12 +80,59 @@
                     })
                 this.$router.push('/login')
             } else {
-
+                this.$axios.get('/api/user/' + this.userId).then(res => {
+                    this.nickName = res.data.data.nickName
+                    this.phoneNumber = res.data.data.phoneNumber
+                    this.avatar = res.data.data.avatar
+                    this.point = res.data.data.point
+                })
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .topContainer {
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+        background-color: rgb(61,182,243);
+        border-radius: 4px;
+        margin-bottom: 10px;
+        width: 90%;
+        margin-left: 5%;
+        height: 200px
+    }
+    .accountFont {
+        color:white;
+        position: relative;
+        left: 2%;
+        top: 10% ;
+        font-size: 1.5em;
+        margin: 0
+    }
+    .leftContainer {
+        background-color: white;
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+        height: 430px;
+        width: 47%;
+        display: inline-block;
+        border-radius: 4px;
+        position: relative;
+        top: -145px;
+        left: 2%
+    }
+    .rightContainer {
+        background-color: white;
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+        height: 430px;
+        width: 47%;
+        display: inline-block;
+        float: right;
+        border-radius: 4px;
+        position: relative;
+        top: -145px;
+        right: 2%;
+    }
+    .avatar {
+        border-bottom: 1px solid rgb(240,240,240)
+    }
 </style>

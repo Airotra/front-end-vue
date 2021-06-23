cd0be041062b89e573cbdc201f8c35a2012b9cab
MD <md@666.com>
2021-06-23 17:20:38 +0800

    对话框设计：移除表单中某一商品

---
+++ src/pages/main/components/Trolley/TrolleyGoodsRemove.vue
@@ -0,0 +1,41 @@
+<template>
+    <el-dialog
+            title="提示"
+            :visible.sync="dialogVisible"
+            width="30%"
+            :before-close="handleClose">
+        <span>{{info}}</span>
+        <span slot="footer" class="dialog-footer">
+    <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+  </span>
+    </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'TrolleyGoodsDetail',
+  data () {
+    return {
+      dialogVisible: false,
+      info: ''
+    }
+  },
+  methods: {
+    show (remove) {
+      this.dialogVisible = true
+      if (remove === true) {
+        this.info = '移除成功'
+      } else {
+        this.info = '移除失败'
+      }
+    },
+    handleClose () {
+      this.dialogVisible = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

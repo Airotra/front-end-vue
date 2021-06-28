e2e4b2ae0f042e25713e5c3f34f2c41b44ab36fd
syan <1042752003@qq.com>
2021-06-28 12:13:08 +0800

    essential function completed

--- src/pages/main/routes/Trolley/Trolley.vue
+++ src/pages/main/routes/Trolley/Trolley.vue
@@ -74,10 +74,38 @@
     <goods-detail ref="goodsDetail"></goods-detail>
     <goods-remove ref="goodsRemove"></goods-remove>
     <confirm-settlement ref="confirmSettle"></confirm-settlement>
+    <el-dialog
+        title="商品详情"
+        :visible.sync="detailDialogVisible"
+        width="50%"
+        :before-close="handleClose">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-image :src="goodsInfo.picture?url + goodsInfo.picture:defaultImg" class="image2"></el-image>
+        </el-col>
+        <el-col :span="12">
+          <span>商品名: {{ goodsInfo.name }}</span>
+          <el-divider></el-divider>
+          <span>价格: ￥{{ goodsInfo.price }}</span>
+          <el-divider></el-divider>
+          <span>人气值: {{ goodsInfo.purchaseTimes }}</span>
+          <el-divider></el-divider>
+          <span>描述: {{ goodsInfo.description }}</span>
+          <el-divider></el-divider>
+          <span v-if="goodsInfo.sideDec1 !== null&&goodsInfo.sideDec1">额外描述1: {{ goodsInfo.sideDec1 }} <el-divider></el-divider></span>
+          <span v-if="goodsInfo.sideDec2 !== null&&goodsInfo.sideDec2">额外描述2: {{ goodsInfo.sideDec2 }} <el-divider></el-divider></span>
+          <span v-if="goodsInfo.sideDec3 !== null&&goodsInfo.sideDec3">额外描述3: {{ goodsInfo.sideDec3 }} <el-divider></el-divider></span>
+        </el-col>
+      </el-row>
+      <span slot="footer" class="dialog-footer">
+                  <el-button type="primary" @click="detailDialogVisible = false">确 定</el-button>
+                 </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import {getGoods} from '../../../../api/trolleylist'
 import {mapGetters} from 'vuex'
 import {trolleylist} from '@api/trolleylist'
 import {removegoods} from '@api/removegoods'
@@ -102,12 +130,22 @@ export default {
   },
   data () {
     return {
+      defaultImg: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+      url: 'api/file/picture?url=',
+      detailDialogVisible: false,
       TrolleyGoods: [],
       GoodsSelection: [],
       address: '',
       addressId: 0,
       addresslistId: [],
       addresslistdetail: [],
+      goodsInfo: {
+        // 商品拥有的属性
+        name: 'null',
+        price: 'null',
+        purchaseTimes: '0',
+        description: 'null'
+      },
       query: {
         pageNo: 1,
         pageSize: 10,
@@ -124,8 +162,12 @@ export default {
   methods: {
     // 点击获取购物车中商品信息
     handleClick (obj) {
-      console.info(obj)
-      this.$refs.goodsDetail.show(obj)
+      // console.info(obj)
+      getGoods(obj.goodsId).then(res => {
+        console.info(res)
+        this.goodsInfo = res.data
+        this.detailDialogVisible = true
+      })
     },
     // 点击移除购物车中商品
     handleRemove (obj) {
@@ -205,6 +247,14 @@ export default {
     handleCurrentChange (val) {
       this.query.pageNo = val
       this.getTrolley()
+    },
+    handleClose (done) {
+      this.$confirm('确认关闭？')
+          .then(_ => {
+            done()
+          })
+          .catch(_ => {
+          })
     }
   }
 }
